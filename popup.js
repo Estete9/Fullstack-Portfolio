f26860950e2467fb160182f0be7b1e3022852e2a
Esteban <cepalacios@outlook.com>
2023-07-25 10:06:53 -0500

    Add projects array to feed the html dynamically

---
+++ popup.js
@@ -0,0 +1,31 @@
+let popupContent = `
+<section class="popup">
+  <h1 id="popup-title">Tonic</h1>
+  <div class="details-container">
+    <p class="client">CANOPY</p>
+    <img src="/Counter.png" alt="" />
+    <p class="role">Back End Dev</p>
+    <img src="/Counter.png" alt="" />
+    <p class="year">2020</p>
+  </div>
+
+  <img src="Portfolio1.png" alt="" />
+  <p>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since the
+    1500s, when an unknown printer took a galley of type and scrambled it to
+    make a type specimen book. It has survived not only five centuries, but also
+    the leap into electronic typesetting, remaining essent
+  </p>
+  <ul class="tags">
+    <li class="tag">HTML</li>
+    <li class="tag">CSS</li>
+    <li class="tag">JavaScript</li>
+  </ul>
+  <hr class="dividers" />
+  <div class="cta-btns">
+    <button class="cta-btn">See live</button>
+    <button class="cta-btn">See source</button>
+  </div>
+</section>
+`
--- script.js
+++ script.js
@@ -1,29 +1,73 @@
-const menuBtn = document.querySelector('#menu-btn img');
-const menu = document.querySelector('#menu');
+const menuBtn = document.querySelector("#menu-btn img");
+const menu = document.querySelector("#menu");
 let showMenu = false;
-const menuElems = document.querySelectorAll('#menu a');
+const menuElems = document.querySelectorAll("#menu a");
+class Project {
+  constructor(name, description, img, techs, liveVersion, source) {
+    (this.name = name),
+      (this.description = description),
+      (this.img = img),
+      (this.techs = techs),
+      (this.liveVersion = liveVersion),
+      (this.source = source);
+  }
+}
+let projects = [
+  new Project(
+    "Tonic",
+    "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    "assets/works/Snapshoot Portfolio.png",
+    ["HTML", "CSS", "JavaScript"],
+    "/",
+    "/"
+  ),
+  new Project(
+    "Multi-Post Stories",
+    "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
+    "assets/works/Snapshoot Portfolio (3).png",
+    ["HTML", "CSS", "JavaScript"],
+    "/",
+    "/"
+  ),
+  new Project(
+    "Facebook 360",
+    "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    "assets/works/Snapshoot Portfolio (1).png",
+    ["HTML", "CSS", "JavaScript"],
+    "/",
+    "/"
+  ),
+  new Project(
+    "Uber Navigation",
+    "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+    "assets/works/Snapshoot Portfolio (2).png",
+    ["HTML", "CSS", "JavaScript"],
+    "/",
+    "/"
+  ),
+];
 
-menuBtn.style.zIndex = '3';
+menuBtn.style.zIndex = "3";
 
 menuBtn.onclick = function () {
   if (!showMenu) {
-    menu.classList.add('mobile-menu');
-    menu.style.display = 'flex';
-    menuBtn.setAttribute('src', 'assets/header/menu-close.svg');
+    menu.classList.add("mobile-menu");
+    menu.style.display = "flex";
+    menuBtn.setAttribute("src", "assets/header/menu-close.svg");
     showMenu = true;
   } else {
-    menu.classList.remove('mobile-menu');
-    menu.style.display = 'none';
-    menuBtn.setAttribute('src', 'assets/header/menu.svg');
+    menu.classList.remove("mobile-menu");
+    menu.style.display = "none";
+    menuBtn.setAttribute("src", "assets/header/menu.svg");
     showMenu = false;
   }
 };
 
 menuElems.forEach((element) => {
   element.onclick = function () {
-    menu.classList.remove('mobile-menu');
-    menu.style.display = 'none';
-    menuBtn.setAttribute('src', 'assets/header/menu.svg');
+    menu.classList.remove("mobile-menu");
+    menu.style.display = "none";
+    menuBtn.setAttribute("src", "assets/header/menu.svg");
     showMenu = false;
   };
 });
